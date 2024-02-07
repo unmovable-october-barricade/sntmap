65c79092135bf3ad10e1fb376e8f0c33079b437e
GIARNNIA\Hamsterarsch <>
2024-02-07 23:19:43 +0100

    recreated tileset

---
+++ tilesets/1_Generic_Shadowless32x32.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="1_Generic_Shadowless32x32" tilewidth="32" tileheight="32" tilecount="1248" columns="16">
+ <properties>
+  <property name="tilesetCopyright" value="https://limezu.itch.io/"/>
+ </properties>
+ <image source="../../../../Users/Hamsterarsch/Downloads/Modern_Interiors_v41.4/1_Interiors/32x32/Theme_Sorter_Shadowless_32x32/1_Generic_Shadowless32x32.png" width="512" height="2496"/>
+</tileset>
